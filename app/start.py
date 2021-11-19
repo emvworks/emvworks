af49a5ddb9d7e9dca15cdb9ec2a14489c78d753c
emvworks <developer.emvworks@gmail.com>
2021-11-19 22:22:46 +0530

    Third commit

--- app/start.py
+++ app/start.py
@@ -1,1 +1,10 @@
-print('Version 1 installed using USB')
+from machine import Pin
+from time import sleep
+
+print('Version 2 installed using USB')
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.value(not led.value())
+    sleep(0.5)
